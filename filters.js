6684a1d762902304e81a7d0977ce0e10a24888da
Andrea Caldera <andrea.caldera@gmail.com>
2022-10-25 09:56:11 +0200

    adding requested a deployment to timeline filter

--- filters.js
+++ filters.js
@@ -1,5 +1,6 @@
 const PHRASES_TO_HIDE = [
+  "requested a deployment to",
   "temporarily deployed to",
   "had a problem deploying to",
-  "Merge branch 'main' into"
-]
+  "Merge branch 'main' into",
+];
