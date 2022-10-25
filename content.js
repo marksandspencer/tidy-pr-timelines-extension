3528b7d3d50be367b1ced9fd9715a9e820e5dc2e
Matt Woodage <matt.woodage@gmail.com>
2022-10-25 14:34:00 +0100

    linting and cleanup

--- content.js
+++ content.js
@@ -1,161 +1,193 @@
-(function() { 
+/* global chrome, PHRASES_TO_HIDE */
 
-  let isActive = true
-  let numEventsHidden = 0
-  let timeline
-  let pageUrl
-  let monitorInterval
-  const observer = new MutationObserver(checkForTimelineChanges)
+(function () {
+  const itemClass = 'TimelineItem';
+  const itemHiddenClass = 'TimelineItem-Hidden';
 
-  function checkForTimelineChanges(mutations) {
-    // when the dom has changed - check if the timeline has been added to
-    const timelineChanges = mutations.find((mutation) => {
-      return [...mutation.addedNodes].map(m => m.className && m.className.includes('js-timeline-item')).includes(true)
-    })
-    if (timelineChanges) processTimeline()
-  }
+  let isActive = true;
+  let numMatchingEvents = 0;
+  let timeline;
+  let pageUrl;
+  let monitorInterval;
 
-  function loadMoreIfAvailable() {
-    const btn = querySelectorIncludesText("button", "Load more")
-    if (btn) {
-      btn.click()
-      updateButton(true)
-      return true
-    }
-    return false
-  }
+  const showEvent = (el) => {
+    el.classList.remove(itemHiddenClass);
+    el.classList.add(itemClass);
+  };
 
-  function querySelectorIncludesText(selector, text) {
+  const hideEvent = (el) => {
+    el.classList.remove(itemClass);
+    el.classList.add(itemHiddenClass);
+  };
+
+  const querySelectorIncludesText = (selector, text) => {
     return Array.from(document.querySelectorAll(selector)).find((el) =>
       el.textContent.includes(text)
     );
-  }
+  };
 
-  function removePrNoise() {
-    document.querySelectorAll(".TimelineItem").forEach((el) => {
-      const shouldHide = PHRASES_TO_HIDE.find((phrase) => el.innerText.includes(phrase))
-      if (shouldHide) {
-        el.classList.add("TimelineItem-Noise");
-        el.classList.remove("TimelineItem")
-      }
-    });
-    updateButton(false)
-  }
+  const getButton = (create = false) => {
+    const buttonID = 'tidy-timeline-button';
+    const btn = document.getElementById(buttonID);
+    if (btn) {
+      return btn;
+    }
+    if (!create) {
+      return;
+    }
 
-  function getButton(create = false) {
-    const buttonID = 'pr-noise-button'
-    const btn = document.getElementById(buttonID)
-    if (btn) return btn
-    if (!create) return 
-
-    const newBtn = document.createElement("button");
-    newBtn.id = buttonID
-    newBtn.onclick = function() {
-      if (isActive) {
-        deactivate()
-      }
-      else {
-        processTimeline()
-      }
-    };
+    const newBtn = document.createElement('button');
+    newBtn.setAttribute('id', buttonID);
+    // eslint-disable-next-line functional/immutable-data
+    newBtn.onclick = handleClick;
     document.body.appendChild(newBtn);
-    return newBtn
-  }
+    return newBtn;
+  };
 
-  function deactivate() {
-    document.querySelectorAll(".TimelineItem-Noise").forEach((el) => {
-      el.classList.remove("TimelineItem-Noise")
-      el.classList.add("TimelineItem")
+  const getItems = () => {
+    return document.querySelectorAll(`.${itemClass}`);
+  };
+
+  const getHiddenItems = () => {
+    return document.querySelectorAll(`.${itemHiddenClass}`);
+  };
+
+  const updateButton = (isLoading) => {
+    const btn = getButton(true);
+
+    if (isActive) {
+      numMatchingEvents = getHiddenItems().length;
+    }
+
+    const count = `${numMatchingEvents} event${
+      numMatchingEvents === 1 ? '' : 's'
+    }`;
+    const message = isActive ? `${count} hidden` : `Click to hide ${count}`;
+    const loader =
+      isActive && isLoading
+        ? `<img src='${chrome.runtime.getURL('images/loader.gif')}' />`
+        : '';
+    const icon = isActive
+      ? `<span><img src='${chrome.runtime.getURL('images/32.png')}' /></span>`
+      : '';
+
+    // eslint-disable-next-line functional/immutable-data
+    btn.innerHTML = icon + message + loader;
+  };
+
+  const loadMoreIfAvailable = () => {
+    const btn = querySelectorIncludesText('button', 'Load more');
+    if (btn) {
+      btn.click();
+      updateButton(true);
+      return true;
+    }
+    return false;
+  };
+
+  const tidyTimeline = () => {
+    isActive = true;
+    getItems().forEach((el) => {
+      const shouldHide = PHRASES_TO_HIDE.find((phrase) =>
+        el.innerText.includes(phrase)
+      );
+      if (shouldHide) {
+        hideEvent(el);
+      }
     });
-    isActive = false
-    updateButton()
-  }
-
-  function updateButton(isLoading) {
-    const btn = getButton(true)
-    let message
-
-    if (!isActive) {
-      message = `Click to hide ${numEventsHidden} event${numEventsHidden === 1 ? '' : 's'}`
+    updateButton(false);
+    const loading = loadMoreIfAvailable();
+    if (!loading) {
+      stopWatching();
     }
-    else {
-      numEventsHidden = document.querySelectorAll(".TimelineItem-Noise").length
-      message = `${numEventsHidden} event${numEventsHidden === 1 ? '' : 's'} hidden`
-      if (isLoading) message += '&nbsp;&nbsp; <img width="20px" src='+chrome.runtime.getURL("images/loader.gif")+' />'
+  };
+
+  const observer = new MutationObserver((mutations) => {
+    const timelineChanges = mutations.find((mutation) => {
+      return [...mutation.addedNodes]
+        .map((m) => m.className && m.className.includes('js-timeline-item'))
+        .includes(true);
+    });
+    if (timelineChanges) {
+      tidyTimeline();
     }
+  });
 
-    btn.innerHTML = (isActive ? '<span class="pr-noise-active" ><img src='+chrome.runtime.getURL("images/32.png")+' /></span> ' : '') + message;
-  }
-
-  function removeButton() {
-    const btn = getButton()
-    if (btn) document.body.removeChild(btn)
-  }
-
-  function startWatchingForTimelineChanges() {
+  const startWatchingForTimelineChanges = () => {
     observer.observe(timeline, {
       attributes: false,
       characterData: false,
       childList: true,
       subtree: true,
       attributeOldValue: false,
-      characterDataOldValue: false
-    })
-  }
+      characterDataOldValue: false,
+    });
+  };
 
-  function stopWatching() {
+  const stopWatching = () => {
     observer.disconnect();
-  }
+  };
 
-  function processTimeline() {
-    isActive = true
-    removePrNoise()
-    const loading = loadMoreIfAvailable()
-    if (!loading) stopWatching()
-  }
+  const deactivate = () => {
+    getHiddenItems().forEach((el) => {
+      showEvent(el);
+    });
+    isActive = false;
+    updateButton();
+  };
 
-  async function monitor() {
-    // detect url changes on github spa. if a pull request timeline is found - process it
-    const newPageUrl = document.location.href
+  const handleClick = () => {
+    if (isActive) {
+      deactivate();
+    } else {
+      tidyTimeline();
+    }
+  };
+
+  const removeButton = () => {
+    const btn = getButton();
+    if (btn) {
+      document.body.removeChild(btn);
+    }
+  };
+
+  const monitor = async () => {
+    const newPageUrl = document.location.href;
     if (newPageUrl !== pageUrl) {
-      // page has changed
-      pageUrl = newPageUrl
+      pageUrl = newPageUrl;
 
-      // give list a chance to load
       await new Promise((res) => setTimeout(res, 500));
 
-      timeline = document.querySelectorAll('.pull-discussion-timeline')[0]
+      timeline = document.querySelectorAll('.pull-discussion-timeline')[0];
 
       if (timeline) {
-        startWatchingForTimelineChanges()
-        processTimeline()
-      }
-      else {
-        stopWatching()
-        removeButton()
+        startWatchingForTimelineChanges();
+        tidyTimeline();
+      } else {
+        stopWatching();
+        removeButton();
       }
     }
-  }
+  };
 
-  function initialise () {
-    monitorInterval = setInterval(monitor, 1000)
-  }
+  const initialise = () => {
+    monitorInterval = setInterval(monitor, 1000);
+  };
 
-  // wait until page has loaded to trigger the listeners
-  window.addEventListener('load', function load(e){
-    window.removeEventListener('load', load, false);
-    initialise()
-  }, false);
+  window.addEventListener(
+    'load',
+    function load() {
+      window.removeEventListener('load', load, false);
+      initialise();
+    },
+    false
+  );
 
-  // listen for tab switching
-  document.addEventListener('visibilitychange', function (event) {
+  document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        //stop monitoring for page change
-        clearInterval(monitorInterval)
+      clearInterval(monitorInterval);
     } else {
-        //resume monitoring for page change
-        initialise()
+      initialise();
     }
   });
-
-})(); 
+})();
--- filters.js
+++ filters.js
@@ -2,6 +2,5 @@ const PHRASES_TO_HIDE = [
   "had a problem deploying to",
   "requested a deployment to",
   "temporarily deployed to",
-  "had a problem deploying to",
   "Merge branch 'main' into",
 ];
