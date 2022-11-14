356ddb4d478891a1ae4e4d61327e4aa78d255476
Abhishek Gupta <abhishek2411@outlook.com>
2022-11-14 12:15:23 +0000

    Format files

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 const getAllPhrases = () => {
-  return PHRASES_TO_HIDE.map((e) => `<h6>*${e}*</h6>`).join(" ");
-}
+  return PHRASES_TO_HIDE.map((e) => `<h6>*${e}*</h6>`).join(' ');
+};
 
-document.getElementById("filterData").innerHTML = getAllPhrases();
+document.getElementById('filterData').innerHTML = getAllPhrases();
