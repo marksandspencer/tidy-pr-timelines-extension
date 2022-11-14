7a83e9f6fd3cba41a2ed9d5c41e7f6fab55f107c
Abhishek Gupta <abhishek2411@outlook.com>
2022-11-14 12:14:23 +0000

    Fix typo

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-const getAllPhases = () => {
+const getAllPhrases = () => {
   return PHRASES_TO_HIDE.map((e) => `<h6>*${e}*</h6>`).join(" ");
 }
 
-document.getElementById("filterData").innerHTML = getAllPhases();
+document.getElementById("filterData").innerHTML = getAllPhrases();
